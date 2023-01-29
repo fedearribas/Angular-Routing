a47a3a563bab6b2e3c778720059e6b137beff523
Federico Arribas <fede.arribas@gmail.com>
2023-01-29 02:07:40 -0300

    added secondary router outlet to display messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Router, Event, NavigationStart, NavigationEnd, NavigationError, Navigat
 
 import { AuthService } from './user/auth.service';
 import { slideInAnimation } from './app.animation';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -25,12 +26,28 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService, private router: Router) {
+  get isMessageDisplayed():  boolean {
+    return this.messageService.isDisplayed;
+  }
+
+  constructor(private authService: AuthService,
+      private router: Router,
+      private messageService: MessageService) {
     router.events.subscribe({
       next: (routerEvent: Event) => {
         this.checkRouterEvent(routerEvent);
       }
     });
+  }
+
+  displayMessages() {
+    this.router.navigate([ { outlets: { popup: ['messages'] } } ]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages() {
+    this.router.navigate([ { outlets: { popup: null } } ]);
+    this.messageService.isDisplayed = false;
   }
 
   checkRouterEvent(routerEvent: Event) {
